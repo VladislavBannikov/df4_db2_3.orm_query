116a0e6ef1c3f934970a934e6cc0ad0356fe36b9
zuko1623 <zuko1623@gmail.com>
2020-06-28 07:33:15 +0300

    solved with Defer and sorting

--- articles/views.py
+++ articles/views.py
@@ -6,10 +6,12 @@ from .models import Article
 
 def articles_list(request):
     template_name = 'articles/news.html'
-    context = {"object_list": Article.objects.select_related("author").select_related("genre").all()}
+    context = {
+        "object_list": Article.objects.select_related("author").select_related("genre").order_by("-published_at").defer(
+            "text", "author__phone")}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    # ordering = '-published_at'
 
     return render(request, template_name, context)
